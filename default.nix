4effc8223fe7ff5d62554fef88f61e0ecdf5ad94
Jonathan Reeve <jon.reeve@gmail.com>
2021-05-17 00:20:34 -0400

    trying to debug nix CI

--- default.nix
+++ default.nix
@@ -2,22 +2,34 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "3f790f22e477d4a833945a9ed4d6e04adbc34747";
+  ribRevision = "81da868a4ecc9a0baf06b26d712e1c65a251e8fb";
+  ribSrc = builtins.fetchTarball {
+    url = "https://github.com/srid/rib/archive/${ribRevision}.tar.gz";
+    sha256 = "1im732kjw1vmnp0k2gx9qnd9hjy0yk5l2acpqk1ykdg447n0f047";
+  };
 
+  gitignoreSrc = builtins.fetchTarball { 
+    url = "https://github.com/hercules-ci/gitignore/archive/c4662e6.tar.gz";
+    sha256 = "1npnx0h6bd0d7ql93ka7azhj40zgjp815fw2r6smg8ch9p7mzdlx";
+  };
+  inherit (import gitignoreSrc { }) gitignoreSource;
 in {
-    # Rib library source to use
-    rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
-    # Cabal project root
-  , root ? ./.
-    # Cabal project name
-  , name ? "jonreevecom"
-  , ...
+# Rib library source to use
+  rib ? ribSrc
+# Cabal project root
+, root ? gitignoreSource ./.
+# Cabal project name
+, name ? "jonreevecom"
+, ...
 }:
 
-let
+let 
+  # Add your Haskell package overrides here
   source-overrides = {
     with-utf8 = builtins.fetchTarball "https://github.com/serokell/haskell-with-utf8/archive/v1.0.0.0.tar.gz";
   };
-in import rib {
-  inherit root name source-overrides;
+in import rib { 
+  inherit root name source-overrides; 
 }
+
+
