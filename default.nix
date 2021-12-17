bcee3e009a6929caa3e3b4ddb9dc70267450fd56
Jonathan Reeve <jonathan.reeve@columbia.edu>
2021-12-16 22:11:33 -0500

    upgrade PyF

--- default.nix
+++ default.nix
@@ -2,12 +2,12 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "3f790f22e477d4a833945a9ed4d6e04adbc34747";
+  ribRevision = "e12af65e5b8dbf13cc2e611f5e8408d83aa0e875";
 
   inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
 in {
     # Rib library source to use
-    rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
+    rib ? builtins.fetchTarball "https://github.com/JonathanReeve/rib/archive/${ribRevision}.tar.gz"
     # Cabal project root
   , root ? gitignoreSource ./.
     # Cabal project name
@@ -18,6 +18,7 @@ in {
 let
   source-overrides = {
     with-utf8 = builtins.fetchTarball "https://github.com/serokell/haskell-with-utf8/archive/v1.0.0.0.tar.gz";
+    PyF = builtins.fetchTarball "https://github.com/guibou/PyF/archive/v0.10.2.0.tar.gz";
   };
 in import rib {
   inherit root name source-overrides;
